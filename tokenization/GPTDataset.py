2f7355436891f891157b0be79b07704543d3ed9c
Calin <onisoralexandrucalin@gmail.com>
2026-09-19 18:37:47 +0300

    Final update on GPTDataset.py: added print statements and comments to improve code clarity

--- tokenization/GPTDataset.py
+++ tokenization/GPTDataset.py
@@ -46,11 +46,33 @@ def create_dataloader(txt, batch_size=4, max_length=256,
 with open("dataset.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 
+vocab_size = 50257
+output_dim = 256
+token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
 
-dataloader = create_dataloader(raw_text, batch_size=8, max_length=4, stride=4, shuffle=False)
-
+#batch_size -> the number of individual text examples(rows)
+#max_length -> the maximum number of tokens in a single text sequence
+#stride -> how many positions the sliding window shifts forward
+max_length = 4
+dataloader = create_dataloader(
+    raw_text, batch_size=8, max_length=max_length,
+    stride=max_length, shuffle=False
+)
 data_iter = iter(dataloader)
 inputs, targets = next(data_iter)
-print("Inputs:\n", inputs)
-print("\nTargets:\n", targets)
+
+
+# print("Token IDs:\n", inputs)
+# print("\nInputs shape:\n", inputs.shape)
+
+token_embeddings = token_embedding_layer(inputs)
+context_length = max_length
+
+pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
+pos_embeddings = pos_embedding_layer(torch.arange(max_length))
+
+print(pos_embeddings.shape)
+input_embeddings = token_embeddings + pos_embeddings
+print(input_embeddings.shape)
+
 
